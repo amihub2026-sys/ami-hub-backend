1d7e85579f1171ee37fd10d368a696e20c852dee
amihub <amihub2026@gmail.com>
2026-07-17 13:59:25 +0530

    adminboostplan

---
+++ src/controllers/boostPlan.controller.js
@@ -0,0 +1,125 @@
+const BoostPlan = require("../models/boostPlan.model");
+
+
+// CREATE
+exports.createBoostPlan = async(req,res)=>{
+
+try{
+
+const plan =
+await BoostPlan.create(req.body);
+
+
+res.json({
+success:true,
+data:plan
+});
+
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
+
+
+
+
+// GET ALL
+
+exports.getBoostPlans = async(req,res)=>{
+
+try{
+
+const plans =
+await BoostPlan.find()
+.sort({createdAt:-1});
+
+
+res.json({
+success:true,
+data:plans
+});
+
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
+
+
+
+
+// UPDATE
+
+exports.updateBoostPlan = async(req,res)=>{
+
+try{
+
+const plan =
+await BoostPlan.findByIdAndUpdate(
+req.params.id,
+req.body,
+{
+new:true
+}
+);
+
+
+res.json({
+success:true,
+data:plan
+});
+
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
+
+
+
+
+// DELETE
+
+exports.deleteBoostPlan = async(req,res)=>{
+
+try{
+
+await BoostPlan.findByIdAndDelete(
+req.params.id
+);
+
+
+res.json({
+success:true
+});
+
+
+}catch(err){
+
+res.status(500).json({
+success:false,
+message:err.message
+});
+
+}
+
+};
