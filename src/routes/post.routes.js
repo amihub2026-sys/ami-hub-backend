8940b3cb7b1330db7dfe4794ccd6a6a4e7a63a75
amihub <amihub2026@gmail.com>
2026-07-23 13:13:35 +0530

    mypostfix

--- src/routes/post.routes.js
+++ src/routes/post.routes.js
@@ -5,35 +5,65 @@ const postController = require("../controllers/post.controller");
 const protect = require("../middlewares/auth.middleware");
 const uploadPostMedia = require("../middlewares/upload.middleware");
 
-router.post("/", protect, postController.createPost);
 
 router.post(
-  "/:id/media",
-  protect,
-  uploadPostMedia.array("media", 10),
-  postController.uploadPostMedia
+"/",
+protect,
+postController.createPost
 );
 
-router.get("/", postController.getPosts);
-
-router.get(
-  "/seller/analytics",
-  protect,
-  postController.getMyPostAnalytics
-);
 
 router.post(
-  "/:postId/view",
-  protect,
-  postController.addPostView
+"/:id/media",
+protect,
+uploadPostMedia.array("media",10),
+postController.uploadPostMedia
 );
+
 
 router.get(
-  "/:postId/analytics",
-  protect,
-  postController.getPostAnalytics
+"/",
+postController.getPosts
 );
 
-router.get("/:id", postController.getPostById);
+
+// ADD THIS HERE
+router.get(
+"/my-posts",
+protect,
+postController.getMyPosts
+);
+
+
+
+router.get(
+"/seller/analytics",
+protect,
+postController.getMyPostAnalytics
+);
+
+
+
+router.post(
+"/:postId/view",
+protect,
+postController.addPostView
+);
+
+
+
+router.get(
+"/:postId/analytics",
+protect,
+postController.getPostAnalytics
+);
+
+
+
+router.get(
+"/:id",
+postController.getPostById
+);
+
 
 module.exports = router;
